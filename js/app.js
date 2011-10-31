7d8c0dacf2b8defae4ae1fb6c7da0ef19fcb3310
James Aitken <loonypandora@gmail.com>
2011-10-31 23:50:43 +0000

    If we're demo'ing - might as well demo a lot of it.

--- js/app.js
+++ js/app.js
@@ -5,5 +5,25 @@ $(document).ready(function() {
         quanta : ["months", "ordinals", "hours", "minutes", "seconds"]
     });
 
+    $('#months').wordclock({
+        quanta : ["months"]
+    });
+
+    $('#ordinals').wordclock({
+        quanta : ["ordinals"]
+    });
+
+    $('#hours').wordclock({
+        quanta : ["hours"]
+    });
+
+    $('#minutes').wordclock({
+        quanta : ["minutes"]
+    });
+
+    $('#seconds').wordclock({
+        quanta : ["seconds"]
+    });
+
 });
 
