cbe28419dc3fe583dc0b5bdaf50d601c5c8dde77
James Aitken <loonypandora@gmail.com>
2013-01-17 18:07:37 +0000

    Improve and simplify the demo

--- js/app.js
+++ js/app.js
@@ -5,24 +5,8 @@ $(document).ready(function() {
         quanta : ["months", "ordinals", "hours", "minutes", "seconds"]
     });
 
-    $('#months').wordclock({
-        quanta : ["months"]
-    });
-
-    $('#ordinals').wordclock({
-        quanta : ["ordinals"]
-    });
-
-    $('#hours').wordclock({
-        quanta : ["hours"]
-    });
-
-    $('#minutes').wordclock({
-        quanta : ["minutes"]
-    });
-
-    $('#seconds').wordclock({
-        quanta : ["seconds"]
+    $('#time-only').wordclock({
+        quanta : ["hours", "minutes", "seconds"]
     });
 
 });
